9c9743c4e64ea4782529d7b8463d612391916668
wa5 <47422872+wa5@users.noreply.github.com>
2021-06-14 19:20:49 +0530

    Create math.js

---
+++ ecma6/import and export/dynamic import abd export/jsmodules/math.js
@@ -0,0 +1,8 @@
+// export function multiplay(number,multiplayer) {
+// return number*multiplayer;
+
+// }
+export  function multiplay(number,multiplayer) {
+     return number*multiplayer;
+    
+     }
